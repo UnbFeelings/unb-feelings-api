d20340f0912845e0b2df158fa38f896ba64c6e72
Harrison Pedro <hpedro1195@gmail.com>
2018-04-04 16:19:35 -0300

    Create documentation for the api. 1/2

--- api/views.py
+++ api/views.py
@@ -14,12 +14,274 @@ from .serializers import (
 
 
 class CourseViewSet(ModelViewSet):
+    """Description: CourseViewSet.
+
+    API endpoint that allows courses to be viewed, created, deleted or edited.
+    """
+    def list(self, request):
+      """
+      API endpoint that allows all courses to be viewed.
+      ---
+      Response example:
+      ```
+      {
+        "count": 6,
+        "next": null,
+        "previous": null,
+        "results": [
+          {
+            "id": 1,
+            "name": "ENGENHARIA"
+          },
+          {
+            "id": 2,
+            "name": "SOFTWARE"
+          },
+          {
+            "id": 3,
+            "name": "ELETRONICA"
+          },
+          {
+            "id": 4,
+            "name": "AEROESPACIAL"
+          },
+          {
+            "id": 5,
+            "name": "ENERGIA"
+          },
+          {
+            "id": 6,
+            "name": "AUTOMOTIVA"
+          }
+        ]
+      }
+      ```
+      """
+      return super(CourseViewSet, self).list(request)
+    
+    def create(self, request):
+      """
+      API endpoint that allows all courses to be created.
+      ---
+      Body example:
+      ```
+      {
+        "name": "MECATRONICA",
+      }
+      ```
+      Response example:
+      ```
+      {
+        "id": 7,
+        "name": "MECATRONICA"
+      }
+      ```
+      """
+      return super(CourseViewSet, self).create(request)
+
+    def destroy(self, request, pk=None):
+        """
+        API endpoint that allows courses to be deleted.
+        """
+        response = super(CourseViewSet, self).destroy(request, pk)
+        return response
+
+    def retrieve(self, request, pk=None):
+        """
+        API endpoint that allows a specific course to be viewed.
+        ---
+        Response example:
+        ```
+        {
+          "id": 7,
+          "name": "MECATRONICA"
+        }
+        ```
+        """
+        response = super(CourseViewSet, self).retrieve(request, pk)
+        return response
+    
+    def partial_update(self, request, pk=None, **kwargs):
+      """
+      API endpoint that allows a course to be partial edited.
+      ---
+      Body example:
+      ```
+      {
+        "name": "CIVIL"
+      }
+      ```
+      Response example:
+      ```
+      {
+        "id": 7,
+        "name": "CIVIL"
+      }
+      ```
+      """
+      response = \
+          super(CourseViewSet, self).partial_update(request, pk, **kwargs)
+      return response
+
+    def update(self, request, pk=None, **kwargs):
+      """
+      API endpoint that allows a course to be edited.
+      ---
+      Body example:
+      ```
+      {
+        "name": "CIVIL"
+      }
+      ```
+      Response example:
+      ```
+      {
+        "id": 7,
+        "name": "CIVIL"
+      }
+      ```
+      """
+      response = \
+          super(CourseViewSet, self).update(request, pk, **kwargs)
+      return response
     permission_classes = (AdminItemPermissions,)
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
 class SubjectViewSet(ModelViewSet):
+    """Description: StudentViewSet.
+
+    API endpoint that allows subjects to be viewed, created, deleted or edited.
+    """
+    
+    def list(self, request):
+      """
+      API endpoint that allows all subjects to be viewed.
+      ---
+      Response example:
+      ```
+      {
+      "count": 3,
+      "next": "http://localhost:8000/api/subjects/?limit=100&offset=100",
+      "previous": null,
+      "results": [
+          {
+              "id": 1,
+              "name": "CÁLCULO 1                     ",
+              "course": 1
+          },
+          {
+              "id": 2,
+              "name": "CÁLCULO 2                     ",
+              "course": 1
+          },
+          {
+              "id": 3,
+              "name": "CÁLCULO 3                     ",
+              "course": 1
+          },
+      }
+      ```
+      """
+      response = super(SubjectViewSet, self).list(request)
+      return response
+
+    def create(self, request):
+        """
+        API endpoint that allows subjects to be created.
+        ---
+        Body example:
+        ```
+        {
+          "name": "CALCULO 4",
+          "course": 2
+        }
+        ```
+        Response example:
+        ```
+        {
+          "id": 4,
+          "name": "CALCULO 4",
+          "course": 2
+        }
+        ```
+        """
+        response = super(SubjectViewSet, self).create(request)
+        return response
+        
+    def destroy(self, request, pk=None):
+        """
+        API endpoint that allows subjects to be deleted.
+        """
+        response = super(SubjectViewSet, self).destroy(request, pk)
+        return response
+    
+    def retrieve(self, request, pk=None):
+        """
+        API endpoint that allows a specific subject to be viewed.
+        ---
+        Response example:
+        ```
+          {
+              "id": 1,
+              "name": "CÁLCULO 1                     ",
+              "course": 1
+          }
+        ```
+        """
+        response = super(SubjectViewSet, self).retrieve(request, pk)
+        return response
+
+    def partial_update(self, request, pk=None, **kwargs):
+        """
+        API endpoint that allows a subject to be partial edited.
+        ---
+        Body example:
+        ```
+        {
+            "id": 1,
+            "name": "CÁLCULO 1",
+            "course": 1
+        }
+        ```
+        Response example:
+        ```
+        {
+            "id": 1,
+            "name": "CÁLCULO 5",
+            "course": 1
+        }
+        ```
+        """
+        response = \
+            super(SubjectViewSet, self).partial_update(request, pk, **kwargs)
+        return response
+    
+    def update(self, request, pk=None, **kwargs):
+        """
+        API endpoint that allows a subject to be edited.
+        ---
+        Body example:
+        ```
+        {
+            "id": 1,
+            "name": "CÁLCULO 5",
+            "course": 1
+        }
+        ```
+        Response example:
+        ```
+        {
+            "id": 1,
+            "name": "CÁLCULO 6",
+            "course": 2
+        }
+        ```
+        """
+        response = super(SubjectViewSet, self).update(request, pk, **kwargs)
+        return response
+
     permission_classes = (AdminItemPermissions,)
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
@@ -189,12 +451,17 @@ class StudentViewSet(ModelViewSet):
 
 
 class TagViewSet(ModelViewSet):
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    #permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 
 class PostViewSet(ModelViewSet):
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    """Description: CourseViewSet.
+
+    API endpoint that allows courses to be viewed, created, deleted or edited.
+    """
+
+    #permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
