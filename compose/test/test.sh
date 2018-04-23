2c4eb97aedf9b7e4ba41a1cf42757ce33258ad34
Victor Arnaud <victorhad@gmail.com>
2018-04-22 23:16:29 -0300

    Fix docker-compose test

--- compose/test/test.sh
+++ compose/test/test.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-echo "Deleting migrations"
-find . -path "*/migrations/*.pyc"  -delete
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-
-echo "Deleting staticfiles"
-find . -path "ferramenta_custos/static/*"  -delete
-
 echo "Creating migrations and insert into psql database"
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+echo "Run the tests"
+python3 manage.py test
