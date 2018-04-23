f874dc394313bf35e83bf4c1ae5cadbfdf8928fb
Victor Arnaud <victorhad@gmail.com>
2018-04-22 22:39:24 -0300

    Fix developer enviroment

--- compose/dev/dev.sh
+++ compose/dev/dev.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-echo "Deleting migrations"
-find . -path "*/migrations/*.pyc"  -delete
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-
-echo "Deleting staticfiles"
-find . -path "ferramenta_custos/static/*"  -delete
-
-ls -la
-
 echo "Creating migrations and insert into psql database"
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-echo "Run the developer server"
+echo "Run the server"
 python3 manage.py runserver 0.0.0.0:8000
