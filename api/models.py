389d18d614797323b212f091ae339693665a8c87
MatheusRich <matheusrichardt@gmail.com>
2018-05-24 21:19:00 -0300

    Add related_name on models
    Co-authored-by: Fabio Teixeira <fabio1079@gmail.com>
    Co-authored-by: ThalissonMelo <tallisonmelo@hotmail.com>

--- api/models.py
+++ api/models.py
@@ -15,10 +15,7 @@ class Campus(models.Model):
 class Course(models.Model):
     name = models.CharField(max_length=100)
     campus = models.ForeignKey(
-        Campus,
-        on_delete=models.CASCADE,
-        related_name='courses'
-    )
+        Campus, on_delete=models.CASCADE, related_name='courses')
 
     def __str__(self):
         return self.name
@@ -26,7 +23,8 @@ class Course(models.Model):
 
 class Subject(models.Model):
     name = models.CharField(max_length=200)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="subjects")
 
     def __str__(self):
         return self.name
@@ -40,7 +38,8 @@ class Student(AbstractUser):
         _('username'),
         max_length=150,
         unique=False,
-        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        help_text=_(
+            'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
         validators=[username_validator],
         error_messages={
             'unique': _("A user with that username already exists."),
@@ -77,9 +76,14 @@ class Post(models.Model):
     )
 
     content = models.CharField(max_length=280)
-    tag = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(Student, on_delete=None)
-    subject = models.ForeignKey(Subject, null=True, blank=True, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, blank=True, related_name="posts")
+    author = models.ForeignKey(Student, on_delete=None, related_name="posts")
+    subject = models.ForeignKey(
+        Subject,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="posts")
     emotion = models.CharField(max_length=1, choices=EMOTIONS, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -87,7 +91,10 @@ class Post(models.Model):
         tags = ['#' + tag.description for tag in self.tag.all()]
         tags_str = '(' + ', '.join(tags) + ')'
 
-        fields = [self.content, tags_str, self.author.username, self.subject, self.emotion]
+        fields = [
+            self.content, tags_str, self.author.username, self.subject,
+            self.emotion
+        ]
         out = ', '.join(map(str, fields))
         return out
 
